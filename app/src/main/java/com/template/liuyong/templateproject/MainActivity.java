b2a1490f84629a00e237a7def04b10e4322c32d5
liuyong <397021707@qq.com>
2018-11-13 17:44:29 +0800

    add null fragment

--- app/src/main/java/com/template/liuyong/templateproject/MainActivity.java
+++ app/src/main/java/com/template/liuyong/templateproject/MainActivity.java
@@ -1,7 +1,12 @@
 package com.template.liuyong.templateproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.template.liuyong.templateproject.datanullfragment.AvoidOnResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv_startActivityforResult).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AvoidOnResult(MainActivity.this).startForResult(SecondActivity.class, new AvoidOnResult.Callback() {
+                    @Override
+                    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+                        Toast.makeText(MainActivity.this,String.valueOf(resultCode),1).show();
+
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/template/liuyong/templateproject/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.template.liuyong.templateproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.template.liuyong.templateproject.R;
+
+/**
+ * @author 创建人 liuyong
+ * @project 项目名称 TemplateProject
+ * @package 包名 com.template.liuyong.templateproject.datanullfragment
+ * @describe 类描述
+ * @time 创建时间 2018/11/13 17:29
+ * @change 修改内容
+ * @changetime 修改时间
+ * @changeauthor 修改人
+ */
+public class SecondActivity extends Activity {
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        findViewById(R.id.tv_content).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SecondActivity.this.finish();
+            }
+        });
+    }
+
+    @Override
+    public void finish() {
+        setResult(100000);
+        super.finish();
+    }
+}
